4c1dc17183f8db3512022ef379d206235666df9a
Mikhail Matz <matz@utexas.edu>
2020-07-09 11:30:35 -0500

    zscanning in compile_chromosomes

--- compile_chromosomes.R
+++ compile_chromosomes.R
@@ -21,8 +21,6 @@ traits=[filename]    Tab-delimited table of trait(s), same as used for RDA_GWAS.
                      all samples, including the hold-out ones. Predictions will be plotted against 
                      the first column in this table.
                      
-minz=2               minimal SNP z-score cutoff 
-
 runGLMnet=TRUE       Whether to re-run elastic net on compiled data for each replicate.
 
 forcePred=FALSE      forces back-prediction of the same dataset (for sanity check)
@@ -62,8 +60,8 @@ forceAlpha=-1
 fa=grep("forceAlpha=",commandArgs())
 if(length(fa)>0) { forceAlpha=as.numeric(sub("forceAlpha=","", commandArgs()[fa])) } 
 
-require(vegan)
-require(ggplot2)
+# require(vegan)
+# require(ggplot2)
 require(glmnet)
 require(scales)
 require(RColorBrewer)
@@ -96,14 +94,13 @@ gnets=function(dat,trait,alpha=0.5) {
 # for r in `seq 1 10`;do ls *bl_rep${r}_10.RData >rep${r}_bl; done
 # ls rep*_bl >reps_bl
 
-# setwd("~/Dropbox/amil_RDA_association_jun2020/RDA_GWAS/PDruns")
-     # infile = "reps_bl"
-     # traits = "../bleach.traits"
-     # outfile="bleach_rerunGlm.RData"
-	 # minz=2
-	 # forcePred=FALSE
-	 # forceAlpha=-1
-  	# runGLMnet=TRUE
+ setwd("~/Dropbox/amil_RDA_association_jun2020/RDA_GWAS/")
+      infile = "reps_rf"
+      traits = "rf.traits"
+      outfile="rf7_noRerun_mz4.RData"
+	  forcePred=FALSE
+	  forceAlpha=-1
+  	  runGLMnet=FALSE
 
 reps=scan(infile,what="character")		
 traits=read.table(traits,header=T,stringsAsFactors=F)
@@ -112,7 +109,7 @@ traits$sample=NULL
 tnames=names(traits)
 
 rr=reps[1]
-preds=list()
+zscan=seq(2,5,0.1);zr=vector("list", length = length(zscan))
 for (r in 1:length(reps)){
 	rr=reps[r]
 
@@ -177,54 +174,56 @@ for (r in 1:length(reps)){
 	}
 	save(out,gt.s,gt.test,sample.scores,file=paste(rr,"_",outfile,".RData",sep=""))
 
-#------------- predicting 
+#------------- predicting - scanning through z-score cutoffs
 
 	if(forcePred) { 
 		gt.test=gt.s 
 		traits.test=traits[colnames(gt.s),]
 		}
-	tt=traits.test[,1]
+	tt=as.numeric(traits.test[,1])
 	tt[which(is.na(tt))]=mean(tt,na.rm=T)
 	
-	pree=c()
-	for (i in 1:ncol(gt.test)) {
-		pree[i]=sum(out$beta.rr[abs(out$zscore)>=minz]*gt.test[abs(out$zscore)>=minz,i])+out$intercept[1]
+	pred.rr=c();pred.lm=c()
+	for (z in 1:length(zscan)) {
+		minz=zscan[z]
+		for (i in 1:ncol(gt.test)) {
+			pred.rr[i]=sum(out$beta.rr[abs(out$zscore)>= minz]*gt.test[abs(out$zscore)>= minz,i])+out$intercept[1]
+			pred.lm[i]=sum(out$beta[abs(out$zscore)>= minz]*gt.test[abs(out$zscore)>= minz,i])
+		}	
+		preds=data.frame(cbind(rr=pred.rr,lm=pred.lm,true=tt))
+		row.names(preds)=row.names(traits.test)
+		preds$rep=rr
+#		zr[[z]][[r]]=append(zr[[z]],preds[[r]])
+		zr[[z]][[r]]=preds
 	}
-		
-	pree3=c()
-	for (i in 1:ncol(gt.test)) {
-		pree3[i]=sum(out$beta[abs(out$zscore)>=minz]*gt.test[abs(out$zscore)>=minz,i])
-	}
-
-	# par(mfrow=c(1,2))
-	# plot(pree~traits.test[,1],main=paste(rr,"reg.reg. betas"))
-	# mtext(round(cor(pree,tt),2))
-	# plot(pree3~traits.test[,1],main=paste(rr,"simple betas"))
-	# mtext(round(cor(pree3,tt),2))
-
-	preds[[r]]=data.frame(cbind(rr=pree,lm=pree3,true=traits.test[,1]))
-	row.names(preds[[r]])=row.names(traits.test)
-	preds[[r]]$rep=rr
-
 }
+pdf(paste(outfile,".pdf",sep=""),width=11,height=3.2)
+par(mfrow=c(1,4))
+zr2=c();Ns=c()
+for (z in 1:length(zscan)) {
+	Ns=append(Ns,sum(abs(out$zscore)>=zscan[z]))
+	allpreds=do.call(rbind,zr[[z]])
+	zr2=append(zr2,cor(allpreds$rr,allpreds$true)^2)
+}
+#plot(zr2~zscan)
+plot(zr2~Ns,xlab="N(SNPs)",ylab="prediction R2")
+lines(zr2~Ns)
 
-N=sum(abs(out$zscore)>=minz)
-allpreds=do.call(rbind,preds)
+best=which(zr2==max(zr2))
+allpreds=do.call(rbind,zr[[best]])
+	
+getPal = colorRampPalette(brewer.pal(9, "Set1"))
+repColors= getPal(length(reps))
+N=Ns[best]
 jitter=0.01*max(allpreds$true)
 allpreds$re.lm=rescale(allpreds$lm,range(allpreds$true))+rnorm(nrow(allpreds),0,jitter)
 allpreds$re.rr=rescale(allpreds$rr,range(allpreds$true))+rnorm(nrow(allpreds),0,jitter)
 allpreds$re.true=allpreds$true+rnorm(nrow(allpreds),0,jitter)
 
-#cols=WGCNA::labels2colors(allpreds$rep)
-getPal = colorRampPalette(brewer.pal(9, "Set1"))
-repColors= getPal(length(reps))
-
-pdf(paste(outfile,".pdf",sep=""),width=8,height=3.2)
-par(mfrow=c(1,3))
-plot(re.lm~re.true,allpreds,main=paste("z:",minz," Nsnps:",N," simple betas"),col= repColors,pch=16)
-mtext(round(cor(allpreds$lm,allpreds$true),2))
-plot(re.rr~re.true,allpreds,main=paste("z:",minz," Nsnps:",N," rr betas"),col= repColors,pch=16)
-mtext(round(cor(allpreds$rr,allpreds$true),2))
+plot(re.lm~re.true,allpreds,main=paste("z:",zscan[best]," Nsnps:",N," simple betas"),col= repColors,pch=16)
+mtext(round(cor(allpreds$lm,allpreds$true)^2,2))
+plot(re.rr~re.true,allpreds,main=paste("z:",zscan[best]," Nsnps:",N," rr betas"),col= repColors,pch=16)
+mtext(round(cor(allpreds$rr,allpreds$true)^2,2))
 plot(lm~rr,allpreds,col= repColors,main="prediction comparison",pch=16)
 dev.off()
 
