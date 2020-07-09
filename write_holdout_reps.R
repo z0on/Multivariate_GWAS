b081e2f8fe0099658a9186d926094a832a17d952
Mikhail Matz <matz@utexas.edu>
2020-07-09 11:49:56 -0500

    write reps

--- write_holdout_reps.R
+++ write_holdout_reps.R
@@ -1,4 +1,4 @@
-  traits = "pd.traits"
+  traits = "bleach.traits"
   bams = "bams.qc"
   ibs="zz8.ibsMat"
 
@@ -25,17 +25,25 @@ dim(ibs)
 
 library(vegan)
 
+head(traits)
+propsym=read.table("~/Dropbox/amil_RDA_association_jun2020/prop_symb.plink.txt",header=T)
+head(propsym)
+pd=propsym[,c(1,5)]
+names(pd)=c("sample","propD")
+
 nsamp=10;r=1
-for (r in 1:100) {
+for (r in 1:50) {
 	test=sample(goods,nsamp)
 	goods.use=goods[!(goods %in% test)]
 	ibs.r=ibs[goods.use,goods.use]
 	pp0=capscale(ibs.r~1)
-	mds=cbind(sample=row.names(pp0$CA$u),pp0$CA$u[,1:7])
-	write.table(mds,file=paste("mds7_",r,"_",nsamp,sep=""),quote=F,row.names=F)
+#	mds=cbind(sample=row.names(pp0$CA$u),pp0$CA$u[,1:7])
+#	write.table(mds,file=paste("mds7_",r,"_",nsamp,sep=""),quote=F,row.names=F)
 	mds=cbind(sample=row.names(pp0$CA$u),pp0$CA$u[,1:2])
-	write.table(mds,file=paste("mds2_",r,"_",nsamp,sep=""),quote=F,row.names=F)
-	writeLines(test,paste("rep",r,"_",nsamp,sep=""))
+	pdmds=merge(pd,mds,by="sample")
+#	write.table(mds,file=paste("mds2_",r,"_",nsamp,sep=""),quote=F,row.names=F)
+	write.table(pdmds,file=paste("pdmds2_",r,"_",nsamp,sep=""),quote=F,row.names=F)
+	writeLines(test,paste("rep_pdmds2_",r,"_",nsamp,sep=""))
 }
 
 nsamp=25;r=1
