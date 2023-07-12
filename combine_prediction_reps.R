34b5dea6391efb735927d5ac400670e9af02f5ce
Mikhail V Matz <matz@utexas.edu>
2023-07-12 12:27:02 -0500

    Update combine_prediction_reps.R

--- combine_prediction_reps.R
+++ combine_prediction_reps.R
@@ -5,19 +5,24 @@ for (r in 1:25) {
 	reps[[r]]=allpreds
 }
 Reps=do.call(rbind,reps)
-head(Reps)
+
 par(mfrow=c(1,2))
-plot(lm~true,Reps,main="LM")
+plot(lm~jitter(true,0.2),Reps,main="LM")
 abline(h=0,lty=3,col="red")
 abline(v=0,lty=3,col="red")
 mtext(paste("R2 =",round(cor(Reps$lm,Reps$true)^2,2)))
 
-# sign prediction accuracy
-tt=table((Reps$lm>0.2 & Reps$true>0.2)| (Reps$lm<(-0.2) & Reps$true<(-0.2)))
-acc=tt[2]/sum(Reps$true>0.2 | Reps$true<(-0.2))
+# LM sign prediction accuracy
+tt=table((Reps$lm>0 & Reps$true>0)| (Reps$lm<0 & Reps$true<0))
+acc.lm=tt[2]/sum(Reps$true>0 | Reps$true<0)
 
-
-plot(rr~true,Reps,main="Elastic Net")
+plot(rr~jitter(true,0.2),Reps,main="Elastic Net")
 abline(h=0,lty=3,col="red")
 abline(v=0,lty=3,col="red")
 mtext(paste("R2 =",round(cor(Reps$rr,Reps$true)^2,2)))
+# sign prediction accuracy
+tt=table((Reps$rr>0 & Reps$true>0)| (Reps$rr<0 & Reps$true<0))
+acc=tt[2]/sum(Reps$true>0 | Reps$true<0)
+message("Sign prediction accuracy:")
+message("    LM: ",round(acc.lm,2))
+message("GLMnet: ",round(acc,2))
